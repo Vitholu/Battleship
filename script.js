82e4ac7363a65367bacc9683d6b0d31beb31d7af
Vitholu <116558222+Vitholu@users.noreply.github.com>
2022-11-15 13:03:48 +0100

    add more coherent tests

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ function Shipyard(...ships) {
     location: arguments[2],
     hit(...hitDamage) {
       return this.health -= arguments[0]
+    },
+    isSunk() {
+      return true
     }
   }
   if(arguments[3] !== NaN) {
     ship.hit(arguments[3])
+  }
+  if(ship.health === 0) {
+    return ship.isSunk()
   }
   return ship
 }
--- script.test.js
+++ script.test.js
@@ -1,15 +1,9 @@
 const {Shipyard } = require('./script.js')
 
-test("Shipyard creates ship objects", () => {
-  ship = {
-    name: "Freedom",
-    health: 2,
-    location: undefined,
-    hit: undefined,
-  }
-  expect(Shipyard("Freedom", 2, undefined, undefined)).toMatchObject(ship)
-}) 
-
 test("Hit, decreases health property", () => {
   expect(Shipyard("Freedom", 2, undefined, 1)).toStrictEqual(expect.objectContaining({health: 1}))
+})
+
+test("If health is 0, ship is sunk", () => {
+  expect(Shipyard("Freedom",2, undefined, 2)).toBe(true)
 })
